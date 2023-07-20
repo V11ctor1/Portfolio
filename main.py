ed2cabeb1dd1ada61b949bca81e37f1b5ccde40c
iV <z@z>
2023-07-20 22:49:37 +0300

    Docs Ready

--- main.py
+++ main.py
@@ -9,57 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Портфолио', posit=posit, about=about,
-                           skills_list_hard=skills_list_hard, skills_list_soft=skills_list_soft
-                           )
-
-
-@app.errorhandler(404)
-def http_404_error(error):
-    return redirect('/error404')
-
-
-@app.route('/error404')
-def well():  # колодец
-    return render_template('well.html', title='Вот это поворот 404', year_app='Никогда', b='/index')
-
-
-@app.route('/works')
-def works():
-    return render_template('works.html', title='Работа', year_app='июль 2023', b='/index')
-
-
-@app.route('/weather_form', methods=['GET', 'POST'])
-def weather_form():
-    message = ''
-    if request.method == 'GET':
-        return render_template('weather_form.html', title="Погода", year_app='июль 2023', b='/index')
-    elif request.method == 'POST':
-        town = request.form.get('town')
-        if town == '':
-            town = 'Санкт-Петербург'
-            message = "Вы не ввели город!"
-        key = '6d1cbcd7f1f15faea7b3accc84da2c51'
-        url = 'http://api.openweathermap.org/data/2.5/weather'
-        params = {'APPID': key, 'q': town, 'units': 'metric'}
-        result = requests.get(url, params=params)
-        weather = result.json()  # превратить ответ в формат json
-        code = weather['cod']  # если города нет, поймать ошибку
-        if code != '401':
-            if code != '404':
-                icon = weather['weather'][0]['icon']
-                temp = int(weather['main']['temp'])
-                vlag = weather['main']['humidity']
-                text = weather['weather'][0]['description']
-                return render_template('weather.html', title=f'Погода в городе {town}', year_app='июль 2023',
-                                       town=town, icon=icon, temp=temp, vlag=vlag, text=text,
-                                       message=message, b='/weather_form')
-
-            else:
-                return redirect('/error404')
-
-
-if __name__ == '__main__':
     posit = 'Python Developer'
     about = ''' 
     Умею гуглить и разбираться в сути вещей. 
@@ -103,4 +52,57 @@ if __name__ == '__main__':
                'about': about}
     doc.render(context)
     doc.save("static/doc/resume-r.docx")
+    return render_template('index.html', title='Портфолио', posit=posit, about=about,
+                           skills_list_hard=skills_list_hard, skills_list_soft=skills_list_soft,
+                           experience_list=experience_list
+                           )
+
+
+@app.errorhandler(404)
+def http_404_error(error):
+    return redirect('/error404')
+
+
+@app.route('/error404')
+def well():  # колодец
+    return render_template('well.html', title='<ERROR 404> Вот это поворот! Никогда не было и вот опять!',
+                           year_app='Никогда', b='/index')
+
+
+@app.route('/works')
+def works():
+    return render_template('works.html', title='Ведутся работы', year_app='В процессе', b='/index')
+
+
+@app.route('/weather_form', methods=['GET', 'POST'])
+def weather_form():
+    message = ''
+    if request.method == 'GET':
+        return render_template('weather_form.html', title="Погода", year_app='июль 2023', b='/index')
+    elif request.method == 'POST':
+        town = request.form.get('town')
+        if town == '':
+            town = 'Санкт-Петербург'
+            message = "Вы не ввели город!"
+        key = '6d1cbcd7f1f15faea7b3accc84da2c51'
+        url = 'http://api.openweathermap.org/data/2.5/weather'
+        params = {'APPID': key, 'q': town, 'units': 'metric'}
+        result = requests.get(url, params=params)
+        weather = result.json()  # превратить ответ в формат json
+        code = weather['cod']  # если города нет, поймать ошибку
+        if code != '401':
+            if code != '404':
+                icon = weather['weather'][0]['icon']
+                temp = int(weather['main']['temp'])
+                vlag = weather['main']['humidity']
+                text = weather['weather'][0]['description']
+                return render_template('weather.html', title=f'Погода в городе {town}', year_app='июль 2023',
+                                       town=town, icon=icon, temp=temp, vlag=vlag, text=text,
+                                       message=message, b='/weather_form')
+
+            else:
+                return redirect('/error404')
+
+
+if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
