16d696aca7579688f00e252dc44c511d453b0b87
Student <9723903@mail.ru>
2023-07-20 13:52:11 +0300

    Load docs

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from flask import Flask, redirect
 from flask import render_template, request
 import requests
-app = Flask(__name__)
+from docxtpl import DocxTemplate
 
+app = Flask(__name__)
+posit = 'Python Developer'
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Портфолио')
+    return render_template('index.html', title='Портфолио', posit=posit, about=about)
 
-
-@app.errorhandler(404)
-def http_404_error(error):
-    return redirect('/error404')
-
-
-@app.route('/error404')
-def well():  # колодец
-    return render_template('well.html', title='Вот это поворот 404', year_app='Никогда', b='/index')
-
+#
+# @app.errorhandler(404)
+# def http_404_error(error):
+#     return redirect('/error404')
+#
+#
+# @app.route('/error404')
+# def well():  # колодец
+#     return render_template('well.html', title='Вот это поворот 404', year_app='Никогда', b='/index')
+#
 
 @app.route('/works')
 def works():
@@ -29,7 +31,7 @@ def works():
 def weather_form():
     message = ''
     if request.method == 'GET':
-            return render_template('weather_form.html', title="Погода", year_app='июль 2023', b='/index')
+        return render_template('weather_form.html', title="Погода", year_app='июль 2023', b='/index')
     elif request.method == 'POST':
         town = request.form.get('town')
         if town == '':
@@ -56,4 +58,17 @@ def weather_form():
 
 
 if __name__ == '__main__':
+    posit = 'Python Developer'
+    about = ''' 
+    Умею гуглить и разбираться в сути вещей. 
+    Наше время ограничено, я хочу провести его с максимальной пользой.
+    Беспощадно расставляю приоритеты.
+    Умею работать в команде и быстро адаптироваться к меняющейся обстановке.
+    Суть бизнеса — это построение классных продуктов, за которые пользователи готовы платить.
+    Суть жизни — это жизнь, которой можно гордиться, рядом с любимыми людьми, занимаясь любимым делом.
+    '''
+    doc = DocxTemplate("static/doc/resume.docx")
+    context = {'position': posit, 'experience_list': "t", 'skills_list': "t", 'about': about}
+    doc.render(context)
+    doc.save("static/doc/resume-r.docx")
     app.run(host='127.0.0.1', port=5000, debug=True)
